f4a08bed6b4f7a525c1ac35b30485e01fb86621b
LogaMathesh <matheshloga2006@gmail.com>
2026-04-30 20:45:07 +0530

    Fix Supabase pooler connection for psycopg

--- api/database.py
+++ api/database.py
@@ -1,14 +1,21 @@
 import os
 import psycopg
 from dotenv import load_dotenv
+from urllib.parse import parse_qsl, urlencode, urlsplit, urlunsplit
 
 load_dotenv()
 
 DB_URL = os.environ.get("SUPABASE_DB_URL")
 
+def normalize_db_url(db_url):
+    parts = urlsplit(db_url)
+    query = parse_qsl(parts.query, keep_blank_values=True)
+    filtered_query = [(key, value) for key, value in query if key != "pgbouncer"]
+    return urlunsplit((parts.scheme, parts.netloc, parts.path, urlencode(filtered_query), parts.fragment))
+
 def get_connection():
     if DB_URL:
-        return psycopg.connect(DB_URL, autocommit=False)
+        return psycopg.connect(normalize_db_url(DB_URL), autocommit=False, prepare_threshold=None)
 
     return psycopg.connect(
         dbname=os.environ.get("DB_NAME", "postgres"),
